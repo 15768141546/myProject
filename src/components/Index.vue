7550c8b2bee1e45c151c3b628121ff6befbd3667
lym <1710819221@qq.com>
2018-12-18 18:57:06 +0800

    test1

---
+++ src/components/Index.vue
@@ -0,0 +1,57 @@
+<template>
+	<div class="hello">
+		<el-container>
+			<el-aside width="200px">Aside</el-aside>
+			<el-container>
+				<el-header>Header</el-header>
+				<el-main>Main</el-main>
+			</el-container>
+		</el-container>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Index',
+		data() {
+			const item = {
+				date: '2016-05-02',
+				name: '王小虎',
+				address: '上海市普陀区金沙江路 1518 弄'
+			};
+			return {
+				tableData: Array(20).fill(item)
+			}
+		}
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	h1,
+	h2 {
+		font-weight: normal;
+	}
+	
+	ul {
+		list-style-type: none;
+		padding: 0;
+	}
+	
+	li {
+		display: inline-block;
+		margin: 0 10px;
+	}
+	
+	a {
+		color: #42b983;
+	}
+	
+	.el-header {
+		background: pink;
+	}
+	
+	.el-aside {
+		background: green;
+	}
+</style>
